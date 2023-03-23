ebe6886f55bd9c663ea633a08c19f83e8fc78754
cyPark95 <cas15987@gmail.com>
2023-03-23 14:35:06 +0900

    feat: 회원 도메인 구현

---
+++ Spring-Basic/core/src/main/java/hello/core/member/entity/Member.java
@@ -0,0 +1,28 @@
+package hello.core.member.entity;
+
+import hello.core.member.repository.Grade;
+
+public class Member {
+
+    private Long id;
+    private String name;
+    private Grade grade;
+
+    public Member(Long id, String name, Grade grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+}
