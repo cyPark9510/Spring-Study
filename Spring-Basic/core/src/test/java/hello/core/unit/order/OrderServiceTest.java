ffda969d7965615af56dd6aa1cadeda896eb652f
cyPark95 <cas15987@gmail.com>
2023-03-23 16:19:02 +0900

    test: 주문 도메인 테스트

---
+++ Spring-Basic/core/src/test/java/hello/core/unit/order/OrderServiceTest.java
@@ -0,0 +1,33 @@
+package hello.core.unit.order;
+
+import hello.core.Order.entity.Order;
+import hello.core.Order.service.OrderService;
+import hello.core.Order.service.OrderServiceImpl;
+import hello.core.member.entity.Member;
+import hello.core.member.repository.Grade;
+import hello.core.member.service.MemberService;
+import hello.core.member.service.MemberServiceImpl;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        // given
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        // when
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        // then
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+        assertThat(order.calculatePrice()).isEqualTo(9000);
+    }
+}
