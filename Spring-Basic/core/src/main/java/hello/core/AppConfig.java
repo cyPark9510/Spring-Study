8037fe3d6a70d85bee4cc4b9bddaf13c0347a333
cyPark95 <cas15987@gmail.com>
2023-03-23 20:35:43 +0900

    feat: 스프링으로 전환

--- Spring-Basic/core/src/main/java/hello/core/AppConfig.java
+++ Spring-Basic/core/src/main/java/hello/core/AppConfig.java
@@ -8,21 +8,28 @@ import hello.core.member.repository.MemberRepository;
 import hello.core.member.repository.MemoryMemberRepository;
 import hello.core.member.service.MemberService;
 import hello.core.member.service.MemberServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
