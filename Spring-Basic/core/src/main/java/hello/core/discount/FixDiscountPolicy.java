9ccb3a5833a57db077c6425e25e212cdeefd03a0
cyPark95 <cas15987@gmail.com>
2023-03-31 13:49:14 +0900

    refactor: Autowired 필드명 매칭 적용

--- Spring-Basic/core/src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ Spring-Basic/core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,7 +2,9 @@ package hello.core.discount;
 
 import hello.core.member.entity.Member;
 import hello.core.member.repository.Grade;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000;  // 1000원 할인
