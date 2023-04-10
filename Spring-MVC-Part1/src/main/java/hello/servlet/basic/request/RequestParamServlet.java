c8272ef52b67a5c37b5428bb43a5495da929cb99
cyPark95 <cas15987@gmail.com>
2023-04-10 18:29:40 +0900

    test: HTTP 요청 복수 파라미터 조회

--- Spring-MVC-Part1/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ Spring-MVC-Part1/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -11,13 +11,15 @@ import java.io.IOException;
 /**
  * 1. 파라미터 전송 기능
  * http://localhost:8080/request-param?username=hello&age=20
+ * 2. 동일한 파라미터 전송 가능
+ * http://localhost:8080/request-param?username=hello&username=pcy&age=20
  */
 @WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
 public class RequestParamServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        printParams(request);
+        printMultiParam(request);
     }
 
     private void printParams(HttpServletRequest request) {
@@ -25,5 +27,23 @@ public class RequestParamServlet extends HttpServlet {
         request.getParameterNames().asIterator()
                 .forEachRemaining(paramName -> System.out.println(paramName + ": " + request.getParameter(paramName)));
         System.out.println("[전체 파라미터 조회] - end");
+    }
+
+    private void printParam(HttpServletRequest request) {
+        System.out.println("[단일 파라미터 조회] - start");
+        String username = request.getParameter("username");
+        System.out.println("request.getParameter(\"username\") = " + username);
+        String age = request.getParameter("age");
+        System.out.println("request.getParameter(\"age\") = " + age);
+        System.out.println("[단일 파라미터 조회] - end");
+    }
+
+    private void printMultiParam(HttpServletRequest request) {
+        System.out.println("[이름이 같은 복수 파라미터 조회] - start");
+        String[] usernames = request.getParameterValues("username");
+        for (String name : usernames) {
+            System.out.println("username=" + name);
+        }
+        System.out.println("[이름이 같은 복수 파라미터 조회] - end");
     }
 }
