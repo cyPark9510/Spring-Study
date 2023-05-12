4e0faf63263dd1fe8b2311154fafb21cf1b5b861
cyPark95 <cas15987@gmail.com>
2023-05-12 15:44:54 +0900

    feat: 타임리프 자바스크립트 인라인
    - 기본 기능
    - 반복

--- Spring-MVC-Part2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ Spring-MVC-Part2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -125,6 +125,14 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("newUser", 20));
+        addUsers(model);
+
+        return "basic/javascript";
+    }
+
     private void addUsers(Model model) {
         List<User> list = new ArrayList<>();
         list.add(new User("UserA", 10));
