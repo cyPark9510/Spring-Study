cba5d36f5cb4a31aa9127b535ce6a759091bab67
cyPark95 <cas15987@gmail.com>
2023-06-15 15:27:12 +0900

    feat: 로그인 ArgumentResolver 적용

--- Spring-MVC-Part2/login/src/main/java/hello/login/web/HomeController.java
+++ Spring-MVC-Part2/login/src/main/java/hello/login/web/HomeController.java
@@ -1,14 +1,12 @@
 package hello.login.web;
 
 import hello.login.domain.member.Member;
+import hello.login.web.argumentresolver.Login;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
-
-import static hello.login.web.SessionConst.LOGIN_MEMBER;
 
 @Slf4j
 @Controller
@@ -16,7 +14,7 @@ import static hello.login.web.SessionConst.LOGIN_MEMBER;
 public class HomeController {
 
     @GetMapping("/")
-    public String homeLogin(@SessionAttribute(name = LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+    public String homeLogin(@Login Member loginMember, Model model) {
         // 세션에 회원 데이터가 없으면 home
         if (loginMember == null) {
             return "home";
