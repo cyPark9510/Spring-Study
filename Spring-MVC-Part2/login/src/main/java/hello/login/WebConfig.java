08cc27be80746128f4a2e1faca86ebf04733b5b0
cyPark95 <cas15987@gmail.com>
2023-06-15 15:11:09 +0900

    feat: 인터셉터 등록
    - 필터 제거

--- Spring-MVC-Part2/login/src/main/java/hello/login/WebConfig.java
+++ Spring-MVC-Part2/login/src/main/java/hello/login/WebConfig.java
@@ -1,33 +1,18 @@
 package hello.login;
 
-import hello.login.web.filter.LogFilter;
-import hello.login.web.filter.LoginCheckFilter;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
+import hello.login.web.interceptor.LogInterceptor;
 import org.springframework.context.annotation.Configuration;
-
-import javax.servlet.Filter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public FilterRegistrationBean<Filter> logFilter() {
-        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
-        filterRegistrationBean.setFilter(new LogFilter());
-        filterRegistrationBean.setOrder(1);
-        filterRegistrationBean.addUrlPatterns("/*");
-
-        return filterRegistrationBean;
-    }
-
-    @Bean
-    public FilterRegistrationBean<Filter> loginCheckFilter() {
-        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
-        filterRegistrationBean.setFilter(new LoginCheckFilter());
-        filterRegistrationBean.setOrder(2);
-        filterRegistrationBean.addUrlPatterns("/*");
-
-        return filterRegistrationBean;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
     }
 }
