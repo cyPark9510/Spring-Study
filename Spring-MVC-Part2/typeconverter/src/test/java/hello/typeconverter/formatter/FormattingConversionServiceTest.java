97983adeb25380013b161231ac503d3d1242541a
cyPark95 <cas15987@gmail.com>
2023-06-29 20:47:44 +0900

    test: FormattingConversionService 테스트

---
+++ Spring-MVC-Part2/typeconverter/src/test/java/hello/typeconverter/formatter/FormattingConversionServiceTest.java
@@ -0,0 +1,29 @@
+package hello.typeconverter.formatter;
+
+import hello.typeconverter.converter.IpPortToStringConverter;
+import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.type.IpPort;
+import org.junit.jupiter.api.Test;
+import org.springframework.format.support.DefaultFormattingConversionService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FormattingConversionServiceTest {
+
+    @Test
+    void formattingConversionService() {
+        // given
+        DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+        // 컨버터 등록
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+        // 포맷터 등록
+        conversionService.addFormatter(new MyNumberFormatter());
+
+        // when
+        // then
+        assertThat(conversionService.convert("127.0.0.1:8080", IpPort.class)).isEqualTo(new IpPort("127.0.0.1", 8080));
+        assertThat(conversionService.convert(1000, String.class)).isEqualTo("1,000");
+        assertThat(conversionService.convert("1,000", Number.class)).isEqualTo(1000L);
+    }
+}
